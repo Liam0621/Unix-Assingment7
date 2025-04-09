65edea6bab76e53ec4e7ad95674648c33445110a
Liam0621 <liam.rasmussen1776@gmail.com>
2025-04-09 13:02:09 -0600

    Added tail to keep container running
    Added code after troubleshooting, change made in vim, copied change to local file.

--- App/app_script.sh
+++ App/app_script.sh
@@ -7,3 +7,5 @@ sed -i "s/Welcome to nginx/Welcome to Liam's page/g" /var/www/html/*.html
 sed -i "s/nginx/nginx (pronounced as EngineX)/g" /var/www/html/*.html
 
 service nginx restart
+
+tail -f /dev/null
