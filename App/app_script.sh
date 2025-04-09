3c9fb57e997b94daef288918724b4ae861ec2a35
Liam0621 <liam.rasmussen1776@gmail.com>
2025-04-09 11:03:53 -0600

    Added code to restart nginx service
    Added code to restart nginx

--- App/app_script.sh
+++ App/app_script.sh
@@ -5,3 +5,5 @@ service nginx start
 sed -i "s/Welcome to nginx/Welcome to Liam's page/g" /var/www/html/*.html
 
 sed -i "s/nginx/nginx (pronounced as EngineX)/g" /var/www/html/*.html
+
+service nginx restart
